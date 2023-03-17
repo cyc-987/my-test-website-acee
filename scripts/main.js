a17fff25b64b79b5ef0199135be70c920275439c
cyc <2516803593@qq.com>
2023-03-17 20:19:09 +0800

    0317

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+//alert('hello!');
+document.querySelector("html").addEventListener("click", () => { alert('don\'t touch me!!') })
+let myButton = document.querySelector('button');
+function setUserName() {
+    let myName = prompt('please enter your name:');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Hello, ' + myName + '!';
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hello, ' + storedName + '!';
+}
+myButton.onclick = function () {
+    setUserName();
+}
